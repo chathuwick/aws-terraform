0defe1974eeb8b9b51cf1497d6b28b624d57acc5
Wickramasinghe <UWICKCH@PEROOT.COM>
2018-08-23 12:26:34 +0530

    Adding IGW to vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -156,10 +156,10 @@ resource "aws_route_table" "PrivateRTB" {
   vpc_id = "${aws_vpc.my-vpc.id}"
   route{
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.k8-igw.id}"
+    gateway_id = "${aws_internet_gateway.Internet-Gateway.id}"
   }
   tags {
-    name = "k8-public-route-${var.cluster_name}"
+    name = "k8-public-route-${var.tag_name}"
   }
 }
 
